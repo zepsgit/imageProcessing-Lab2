dc034913addf62b0c1655629214d920ebddee727
ChaseCode <zepengchen423@gmail.com>
2021-06-21 19:13:40 -0230

    add sobel filter

--- lab2.py
+++ lab2.py
@@ -24,8 +24,16 @@ def gkern(l, sig):
     return kernel / np.sum(kernel)
 k3=gkern(3,0.5)
 k4=gkern(13,1.2)
+
+k6=[[1,0,1],[-2,0,2],[-1,0,1]]
+k7=[[-1,-2,-1],[0,0,0],[1,2,1]]
+k8 =[[0.4038,    0.8021,    0.4038]
+    [0.8021,   -4.8233,    0.8021]
+    [0.4038,    0.8021,    0.4038]]
+
+
 #convolve the mask over the image
-'''
+
 def filter(kx):
     kernel_n=kx.shape[0]
     kn=int((kernel_n-1)/2)
@@ -40,7 +48,8 @@ def filter(kx):
                     sum_p=sum_p+img_new[s-kn+p,t-kn+q]*kx[p,q]
                     img_[s-kn,t-kn]=sum_p
     return img_
-    '''
+
+'''
 #without padding
 def filter(kx):
     kernel_n=kx.shape[0]
@@ -54,24 +63,26 @@ def filter(kx):
                     sum_p=sum_p+img[s-kn+p,t-kn+q]*kx[p,q]
                     img_new[s,t]=sum_p
     return img_new
-
+'''
 img_one3=filter(k1)
 img_one3= img_one3.astype(np.uint8)
+#compare the intensities between after padding and origin
 print('scale of origin')
 print(img.min(), img.max())
 print('scale after filter')
+
 print(img_one3.min(), img_one3.max())
 cv2.imwrite('ones3.png', img_one3)
 cv2.imshow('ones3',img_one3)
 
 img_one7=filter(k2)
-img_one7 = img_one3.astype(np.uint8)
+img_one7 = img_one7.astype(np.uint8)
 cv2.imwrite('ones7.png', img_one7)
 cv2.imshow('ones7',img_one7)
 
 img_g3=filter(k3)
 img_g3=img_g3.astype(np.uint8)
-cv2.imshow('g7',img_g3)
+cv2.imshow('g3',img_g3)
 
 img_g7=filter(k4)
 img_g7=img_g7.astype(np.uint8)
---
+++ med.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('img1.png',0)
+m,n=img.shape
+
+def medf(size):
+    #listK=np.zeros([size,size])
+    listK=np.zeros([size,size],dtype=np.uint8)
+    kn=int((size-1)/2)
+    img_=np.zeros([m,n],dtype=np.uint8)
+    img_new=np.zeros([m+size-1,n+size-1],dtype=np.uint8)
+    for s in range(kn,kn+m):
+        for t in range(kn,kn+n):
+            img_new[s,t]=img[s-kn,t-kn]
+            for p in range(0,size):
+                for q in range(0,size):
+                    listK[p,q]=img_new[s-kn+p,t-kn+q]
+                    listK=listK.flatten()
+                    listKsort=np.sort(listK)
+                    med=listKsort[int((size*size-1)/2)]
+                    img_[s-kn,t-kn]=med
+    return img_
+
+m3=medf(3)
+m3=m3.astype(np.uint8)
+cv2.imshow(m3)
+cv2.waitKey(0)
+cv2.destroyAllWindows
